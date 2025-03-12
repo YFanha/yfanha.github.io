459e8a83e09a8d71b0fc9d057623339752ce0e77
YFanha <yannfnha@gmail.com>
2025-03-12 14:27:10 +0100

    feat: add Portfolio page

---
+++ static/js/script.js
@@ -0,0 +1,56 @@
+//page-anim.js
+//iife version of basic animation and navigation (without History API)
+var app = (function(){
+    let pages = [];
+    let links = [];
+    let currentIndex = 0;
+    let root_vars = "";
+    
+    document.addEventListener("DOMContentLoaded", function(){
+        root_vars = document.querySelector(':root');
+        pages = document.querySelectorAll('[data-page]');
+        links = document.querySelectorAll('[data-role="link"]');
+        [].forEach.call(links, function(link){
+            link.addEventListener("click", navigate)
+        });
+    
+        pages[currentIndex].classList.add("active");
+        links[currentIndex].classList.add("active");
+    
+    });
+    
+    function navigate(ev){
+        ev.preventDefault();
+        let id = ev.currentTarget.href.split("#")[1];
+    
+        let newIndex = [...pages].findIndex(page => page.id === id);
+    
+        let currentPage = pages[currentIndex];
+        let nextPage = pages[newIndex];
+    
+        // Change direction of transform animation
+        let direction = currentIndex > newIndex ? "100%" : "-100%";
+    
+        root_vars.style.setProperty('--direction', direction);
+    
+        currentPage.classList.remove('active');
+        currentPage.classList.add('hidden')
+    
+        links[currentIndex].classList.remove('active');
+        links[newIndex].classList.add('active');
+    
+        currentPage.classList.remove('hidden')
+        nextPage.classList.add('active');
+    
+        currentIndex = newIndex;
+        
+        return false;
+    }
+    
+    return {
+        pages,
+        links,
+        xhr: ajax
+    }
+    })();
+    
