595080da9aec80ca29ec57312337cae1a7972e5d
Yann Fanha <yann@fanha.ch>
2025-06-06 21:51:53 +0200

    feat: implement theme toggle functionality and update styles for light/dark modes

--- static/js/script.js
+++ static/js/script.js
@@ -8,6 +8,19 @@ var app = (function(){
         root_vars = document.querySelector(':root');
         pages = document.querySelectorAll('[data-page]');
         links = document.querySelectorAll('[data-role="link"]');
+        const toggleSwitch = document.querySelector("[data-theme-toggle]");
+        console.log("Toggle switch checked:", toggleSwitch.checked);
+
+        toggleSwitch.addEventListener("change", function() {
+            // if checked, set dark mode
+            
+            if (toggleSwitch.checked) {
+                document.querySelector("html").setAttribute("data-theme", "dark");
+            }
+            else{
+                document.querySelector("html").setAttribute("data-theme", "light");
+            }
+        });
         
         [].forEach.call(links, function(link){
             link.addEventListener("click", navigate)
